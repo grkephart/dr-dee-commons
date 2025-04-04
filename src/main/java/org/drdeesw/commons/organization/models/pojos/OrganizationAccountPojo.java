3775d0cde1a9366bbd67bb9efb748b4f71a947b5
Gary Kephart <gkephart@apptricity.com>
2025-04-04 16:37:28 -0700

    More

--- src/main/java/org/drdeesw/commons/organization/models/pojos/OrganizationAccountPojo.java
+++ src/main/java/org/drdeesw/commons/organization/models/pojos/OrganizationAccountPojo.java
@@ -4,6 +4,7 @@
 package org.drdeesw.commons.organization.models.pojos;
 
 
+import org.drdeesw.commons.accounting.models.pojos.BaseAccountPojo;
 import org.drdeesw.commons.security.models.pojos.UserPojo;
 
 
@@ -11,8 +12,9 @@ import org.drdeesw.commons.security.models.pojos.UserPojo;
  * ChatGPT's version as of 2025-02-09 11:17 PM PST
  */
 @SuppressWarnings("serial")
-public class OrganizationAccountPojo extends
-    AbstractOrganizationAccountPojo<UserPojo, OrganizationAccountHolderPojo, OrganizationAccountProviderPojo>
+public class OrganizationAccountPojo extends AbstractOrganizationAccountPojo<UserPojo, //
+    BaseOrganizationAccountHolderPojo<UserPojo, ? extends BaseAccountPojo<UserPojo, ?, ?>>, //
+    BaseOrganizationAccountProviderPojo<UserPojo, ? extends BaseAccountPojo<UserPojo, ?, ?>>>
 {
   public OrganizationAccountPojo()
   {
